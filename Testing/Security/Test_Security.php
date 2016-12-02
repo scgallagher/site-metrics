2cf1ab0994f59e974ff9e14ddf4f7ac164f6b067
scgallagher <seangallagher135@gmail.com>
2016-12-02 00:22:42 -0500

    finished SSL scan

--- Testing/Security/Test_Security.php
+++ Testing/Security/Test_Security.php
@@ -3,8 +3,9 @@
 require_once("Test_Security_ScanController.php");
 include_once("firephp-core-0.4.0/lib/FirePHPCore/fb.php");
 
+//$scanController = new Test_Security_ScanController("https://github.com");
 $scanController = new Test_Security_ScanController("https://www.nhl.com");
-// $scanController = new Test_Security_ScanController("localhost");
+//$scanController = new Test_Security_ScanController("localhost");
 $results = $scanController->scan();
 echo "Done\n";
 

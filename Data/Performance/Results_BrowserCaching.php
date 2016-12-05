cea831bc4d779bc83fd283b449f3508c3ae29f26
root <seangallagher135@gmail.com>
2016-12-05 12:07:01 -0500

    added proper variables to results for browser caching

--- Data/Performance/Results_BrowserCaching.php
+++ Data/Performance/Results_BrowserCaching.php
@@ -3,6 +3,9 @@
 	class Results_BrowserCaching {
 
 		public $testPassed;
+		public $browserCachingResult;
+		public $cookiesSizeInBytes;
+		public $cookiesCount;
 
 		public function __toString(){
 			$output = "";
@@ -12,6 +15,9 @@
 		public function parseJSON(){
 			$results = array();
 			$results["testPassed"] = $this->testPassed;
+			$results["cookiesSizeInBytes"] = $this->cookiesSizeInBytes;
+			$results["cookiesCount"] = $this->cookiesCount;
+			$results["browserCachingResult"] = $this->browserCachingResult;
 			return $results;
 		}
 

446b9610c08a0c32dde02d2b039c886a0bd6413f
Louis Scola <Louis Scola>
2016-12-05 11:08:56 -0500

    untested pageLoad and browserCaching metrics

--- Scan/Performance/Scan_BrowserCaching.php
+++ Scan/Performance/Scan_BrowserCaching.php
@@ -5,22 +5,72 @@
 	class Scan_BrowserCaching {
 
 		private $dom;
+		private $url;
 		private $resultsBrowserCaching;
 
-		public function __construct($dom){
+		public function __construct($dom, $url){
 			$this->dom = $dom;
+			$this->url = $url;
 			$this->resultsBrowserCaching = new Results_BrowserCaching();
 		}
 
 		public function scan(){
+			$url = $this->url;
+			$cookies = $this->getLandingCookies($url);
+			$cookiesStr = implode(" ", $cookies);
+			
+			$this->resultsBrowserCaching->cookiesSizeInBytes = strlen($cookiesStr);
+			$this->resultsBrowserCaching->cookiesCount = count($cookies);
+			
+			$grade = 0; //out of 3 possible points
+			if($this->resultsBrowserCaching->cookiesCount >= 50)
+				$grade += 0;
+			elseif($this->resultsBrowserCaching->cookiesCount >= 40)
+				$grade += 1;
+			elseif($this->resultsBrowserCaching->cookiesCount < 40)
+				$grade += 2;
+			
+			if($this->resultsBrowserCaching->cookiesSizeInBytes < 4000)
+				$grade += 1;
+			elseif($this->resultsBrowserCaching->cookiesSizeInBytes >= 4000)
+				$grade = 0; //this is the standard max cookie size and is seen as an automatic failure
+				
+			if($grade == 3)
+				$this->resultsBrowserCaching->browserCachingResult = "Good";
+			elseif($grade == 2)
+				$this->resultsBrowserCaching->browserCachingResult = "Ok";
+			else
+				$this->resultsBrowserCaching->browserCachingResult = "Bad";
+		
 			$this->resultsBrowserCaching->testPassed = $this->testPassed();
 			return $this->resultsBrowserCaching;
 		}
 
 		public function testPassed(){
+			if (strtolower($this->resultsBrowserCaching->browserCachingResult) == "good")
+			{
+				return true;
+			}
+
 			return false;
 		}
 
+		
+		
+		private getLandingCookies($url){
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_HEADER, 1);
+			$result = curl_exec($curl);
+			preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+			$cookies = array();
+			foreach($matches[1] as $item) {
+				parse_str($item, $cookie);
+				$cookies = array_merge($cookies, $cookie);
+			}
+			return $cookies;
+		}
 	}
 
 ?>
--- Scan/Performance/Scan_PageLoad.php
+++ Scan/Performance/Scan_PageLoad.php
@@ -5,22 +5,58 @@
 	class Scan_PageLoad {
 
 		private $dom;
+		private $url;
 		private $resultsPageLoad;
 
-		public function __construct($dom){
+		public function __construct($dom, $url){
 			$this->dom = $dom;
+			$this->url = $url;
 			$this->resultsPageLoad = new Results_PageLoad();
 		}
 
 		public function scan(){
+			$scanTarget = $this->url;
+			$this->resultsPageLoad->loadTimeInSeconds = $this->getRequestTime($scanTarget);
+			
+			if ($this->resultsPageLoad->loadTimeInSeconds < 0)
+				$this->resultsPageLoad->pageLoadResult = "ERROR";
+			elseif ($this->resultsPageLoad->loadTimeInSeconds < 3)
+				$this->resultsPageLoad->pageLoadResult = "Good";
+			elseif ($this->resultsPageLoad->loadTimeInSeconds < 5)
+				$this->resultsPageLoad->pageLoadResult = "Ok";
+			else
+				$this->resultsPageLoad->pageLoadResult = "Bad";
+			
 			$this->resultsPageLoad->testPassed = $this->testPassed();
 			return $this->resultsPageLoad;
 		}
 
 		public function testPassed(){
+			if (strtolower($this->resultsPageLoad->pageLoadResult) == "good")
+			{
+				return true;
+			}
+
 			return false;
 		}
 
+		
+		
+		private function getRequestTime($url){
+			$starttime = microtime(true);
+			$file = @fsockopen($url, 80, $errNumber, $errText, 30);
+			$endtime = microtime(true);
+			
+			$time = 0;
+			if(!$file) {
+				$time = -1;
+			} else {
+				fclose($file);
+				$time = ($endtime - $starttime) * 1000000;
+				$time = round($time, 2);
+			}
+			return $time;
+		}
 	}
 
 ?>

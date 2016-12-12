2287e5de7be7f7f3e1e754025320fded003e321f
scgallagher <seangallagher135@gmail.com>
2016-12-11 22:40:27 -0500

    cleaned up PluginMain.php

--- PluginMain.php
+++ PluginMain.php
@@ -13,14 +13,4 @@
 		echo "<script>window.open(\"wp-content/plugins/site-metrics/UI/interface.html?email=" . $email . "\");</script>";
 	}
 
-	function load_js(){
-		echo "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n";
-		echo "<script type=\"text/javascript\" src=\"wp-content/plugins/site-metrics/UI/toolbar.js\"></script>\n";
-	}
-
-	function load_css(){
-		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"wp-content/plugins/site-metrics/UI/toolbarStyle.css\">\n";
-		echo "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css\">\n";
-	}
-
 ?>
